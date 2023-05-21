28bd26d6844c65a0679528828083770db9bd745d
fabian <dechent.fabian@gmail.com>
2023-05-21 17:45:00 +0200

    path adapted

--- dmb/data/dim_2/create2.py
+++ dmb/data/dim_2/create2.py
@@ -34,9 +34,9 @@ if __name__ == "__main__":
 
 
     parser = argparse.ArgumentParser(description='Run worm simulation for 2D BH model')
-    parser.add_argument('--number_of_samples', type=int, default=25,
+    parser.add_argument('--number_of_samples', type=int, default=1,
                         help='number of samples to run')
-    parser.add_argument('--number_of_jobs', type=int, default=10,
+    parser.add_argument('--number_of_jobs', type=int, default=1,
                         help='number of jobs to run in parallel')
     
     args = parser.parse_args()
@@ -59,7 +59,8 @@ if __name__ == "__main__":
         sim = WormSimulation(p,save_dir=save_dir)
 
         sim.save_parameters()
-        sim.run_until_convergence(executable="/u/bale/paper/worm/build_non_uniform/qmc_worm_mpi")
+        # sim.run_until_convergence(executable="/u/bale/paper/worm/build_non_uniform/qmc_worm_mpi")
+        sim.run_until_convergence(executable="/Users/fabian/paper/worm/build_non_uniform/qmc_worm_mpi")
 
         gc.collect()
 
