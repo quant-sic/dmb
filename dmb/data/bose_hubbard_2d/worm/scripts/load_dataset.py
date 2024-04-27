7f7e575dec22e74533948daee39b4e514f7e9885
sherbig <sandro.herbig@mpl.mpg.de>
2024-04-27 23:43:55 +0200

    dataset loading

---
+++ dmb/data/bose_hubbard_2d/worm/scripts/load_dataset.py
@@ -0,0 +1,277 @@
+def get_simulation_valid(
+    simulation_dir: Path,
+    redo: bool = False,
+    log_fn: callable = None,
+    check_readable: bool = True,
+) -> bool:
+
+    if check_readable:
+        try:
+            sim = WormSimulation.from_dir(simulation_dir)
+        except (OSError, KeyError, ValueError):
+            if log_fn:
+                log_fn(f"Could not load simulation from {simulation_dir}", "error")
+            return False
+
+    if "clean" in sim.record and not sim.record["clean"] and not redo:
+        return False
+    elif "clean" in sim.record and sim.record["clean"] and not redo:
+        return True
+    else:
+        valid = sim.valid
+
+    # general purpose validation
+    sim.record["clean"] = valid
+
+    return valid
+
+
+def filter_by_error(
+    sim_dir: Path,
+    max_density_error: float,
+    recalculate_errors: bool = False,
+    log_fn: callable = None,
+) -> bool:
+    try:
+        simulation = WormSimulation.from_dir(sim_dir)
+    except (OSError, KeyError, ValueError):
+        if log_fn:
+            log_fn(f"Could not load simulation from {sim_dir}", "error")
+        return False
+
+    try:
+        if recalculate_errors:
+            if log_fn:
+                log_fn(f"Recalculating errors for {simulation.save_dir}", "info")
+            if len(simulation.record["steps"]) == 0:
+                simulation.record["steps"] = [{"error": None, "tau_max": None}]
+
+            simulation.record["steps"][-1]["error"] = simulation.max_density_error
+            simulation.record["steps"][-1]["tau_max"] = simulation.max_tau_int
+
+        return (simulation.record["steps"][-1]["error"] <= max_density_error) and (
+            simulation.record["steps"][-1]["tau_max"] > 0
+        )
+    except (IndexError, TypeError, KeyError) as e:
+        if log_fn:
+            log_fn(f"Error {e} During error filtering for {simulation}", "error")
+        return False
+
+
+def clean_sim_dirs(
+    self,
+    sim_dirs,
+    redo=False,
+    verbose=False,
+    max_density_error: float | None = None,
+    recalculate_errors: bool = False,
+    delete_unreadable: bool = False,
+    check_readable: bool = True,
+):
+
+    valid_sim_dirs = [
+        get_simulation_valid(
+            sim_dir,
+            redo=redo,
+            log_fn=self.log,
+            check_readable=check_readable,
+        )
+        for sim_dir in sim_dirs
+    ]
+
+    # valid_sim_dirs = ProgressParallel(
+    #     n_jobs=10,
+    #     total=len(sim_dirs),
+    #     desc="Filtering valid simulations",
+    #     use_tqdm=verbose,
+    # )(
+    #     delayed(get_simulation_valid)(sim_dir, redo=redo, log_fn=self.log)
+    #     for sim_dir in sim_dirs
+    # )
+
+    if delete_unreadable:
+        for sim_dir, valid in zip(sim_dirs, valid_sim_dirs):
+            if not valid:
+                self.log(f"Deleting {sim_dir}")
+                shutil.rmtree(sim_dir)
+
+    sim_dirs = list(
+        itertools.compress(
+            sim_dirs,
+            valid_sim_dirs,
+        )
+    )
+
+    if max_density_error is not None:
+        sim_dirs = list(
+            itertools.compress(
+                sim_dirs,
+                [
+                    filter_by_error(
+                        sim_dir,
+                        max_density_error=max_density_error,
+                        recalculate_errors=recalculate_errors,
+                        log_fn=self.log,
+                    )
+                    for sim_dir in sim_dirs
+                ],
+                # ProgressParallel(
+                #     n_jobs=10,
+                #     total=len(sim_dirs),
+                #     desc="Filtering by error",
+                #     use_tqdm=verbose,
+                # )(
+                #     delayed(filter_by_error)(
+                #         sim_dir,
+                #         max_density_error=max_density_error,
+                #         recalculate_errors=recalculate_errors,
+                #         log_fn=self.log,
+                #     )
+                #     for sim_dir in sim_dirs
+                # ),
+            )
+        )
+
+    return sim_dirs
+
+
+def load_sample(simulation_dir, observables, reload=False):
+
+    inputs_path = simulation_dir / "inputs.pt"
+    outputs_path = simulation_dir / "outputs.pt"
+
+    try:
+        sim = WormSimulation.from_dir(simulation_dir)
+        saved_observables = sim.record["saved_observables"]
+    except Exception:
+        reload = True
+
+    if not inputs_path.exists() or not outputs_path.exists() or reload:
+        sim = WormSimulation.from_dir(simulation_dir)
+
+        saved_observables = (
+            sim.observables.observable_names["primary"]
+            + sim.observables.observable_names["derived"]
+        )
+
+        expectation_values = [
+            sim.observables.get_expectation_value(obs_type, obs_name)
+            for obs_type in ["primary", "derived"]
+            for obs_name in sim.observables.observable_names[obs_type]
+        ]
+        expanded_expectation_values = [
+            (
+                np.full(
+                    shape=(sim.input_parameters.Lx, sim.input_parameters.Ly),
+                    fill_value=obs,
+                )
+                if obs.ndim == 0
+                else obs
+            )
+            for obs in expectation_values
+        ]
+        # stack observables
+        outputs = torch.stack(
+            [torch.from_numpy(obs) for obs in expanded_expectation_values],
+            dim=0,
+        )
+
+        inputs = get_nn_input(
+            sim.input_parameters.mu,
+            sim.input_parameters.U_on,
+            sim.input_parameters.V_nn,
+            cb_projection=True,
+            target_density=sim.observables.get_expectation_value("primary", "density"),
+        )
+
+        # save to .npy files
+        torch.save(inputs, inputs_path)
+        torch.save(outputs, outputs_path)
+
+        # save saved_observables
+        sim.record["saved_observables"] = saved_observables
+
+    else:
+        inputs = torch.load(inputs_path)
+        outputs = torch.load(outputs_path)
+
+        # load saved_observables
+        sim = WormSimulation.from_dir(simulation_dir)
+        saved_observables = sim.record["saved_observables"]
+
+        # filter observables
+        outputs = outputs[[saved_observables.index(obs) for obs in observables]]
+
+    metadata = {
+        "max_density_error": sim.max_density_error,
+        "L": sim.input_parameters.Lx,
+        "J": sim.input_parameters.t_hop[0, 0, 0],
+        "U": sim.input_parameters.U_on[0, 0],
+        "mu": sim.input_parameters.mu_offset,
+        "V_nn": sim.input_parameters.V_nn[0, 0, 0],
+    }
+
+    return inputs, outputs, metadata
+
+
+def load_dataset_simulations(
+    simulations_dir: Path,
+    dataset_save_path: Path,
+    include_tune_dirs: bool = False,
+    clean: bool = True,
+    reload: bool = False,
+    verbose: bool = False,
+    max_density_error: float = 0.015,
+    recalculate_errors: bool = False,
+    delete_unreadable: bool = False,
+    check_readable: bool = True,
+    observables: list[str] = [
+        "density",
+        "density_density_corr_0",
+        "density_density_corr_1",
+        "density_density_corr_2",
+        "density_density_corr_3",
+        "density_squared",
+        "density_max",
+        "density_min",
+        "density_variance",
+    ],
+):
+    all_simulation_directories = sorted(simulations_dir.glob("*"))
+
+    if include_tune_dirs:
+        all_simulation_directories += [
+            all_simulation_directories / "tune"
+            for directory in all_simulation_directories
+        ]
+
+    log.info(f"Found {len(all_simulation_directories)} simulation directories.")
+
+    if clean:
+        clean_simulation_directories = clean_sim_dirs(
+            all_simulation_directories,
+            redo=reload,
+            verbose=verbose,
+            max_density_error=max_density_error,
+            recalculate_errors=recalculate_errors,
+            delete_unreadable=delete_unreadable,
+            check_readable=check_readable,
+        )
+
+    else:
+        clean_simulation_directories = all_simulation_directories
+
+    log.info(f"Found {len(clean_simulation_directories)} valid simulation directories.")
+
+    samples_dir = dataset_save_path / "samples"
+    samples_dir.mkdir(exist_ok=True)
+
+    for sim_dir in clean_simulation_directories:
+        inputs, outputs, metadata = load_sample(sim_dir, observables, reload=reload)
+
+        sample_save_path = samples_dir / sim_dir.name
+        torch.save(inputs, sample_save_path / "inputs.pt")
+        torch.save(outputs, sample_save_path / "outputs.pt")
+
+        with open(sample_save_path / "metadata.json", "w") as f:
+            json.dump(metadata, f)
--- dmb/data/dataset.py
+++ dmb/data/dataset.py
@@ -1,15 +1,61 @@
 from abc import ABC, abstractmethod
 
 from torch.utils.data import Dataset
+from attrs import define
+from pathlib import Path
+import json
+import torch
+from typing import Callable
 
 
 class IdDataset(Dataset, ABC):
 
     @abstractmethod
-    def get_ids_from_indices(self, indices: tuple[int,
-                                                  ...]) -> tuple[str, ...]:
-        ...
+    def get_ids_from_indices(self, indices: tuple[int, ...]) -> tuple[str, ...]: ...
 
     @abstractmethod
+    def get_indices_from_ids(self, ids: tuple[str, ...]) -> tuple[int, ...]: ...
+
+
+@define
+class DMBDataset(IdDataset):
+    """Dataset for DMB data.
+
+    Expected directory structure:
+        - Each sample is stored in a separate directory.
+        - The directory name is the sample ID.
+        - The directory contains the following files:
+            - 'inputs.pt': The input tensor.
+            - 'outputs.pt': The output tensor.
+            - 'metadata.json': The metadata dictionary.
+    """
+
+    dataset_dir_path: Path
+    transforms: Callable[
+        [tuple[torch.Tensor, torch.Tensor]], tuple[torch.Tensor, torch.Tensor]
+    ]
+
+    def __attrs_post_init__(self):
+        self.sample_ids = [
+            path.name for path in self.dataset_dir_path.iterdir() if path.is_dir()
+        ]
+
+    def __len__(self) -> int:
+        return len(self.sample_ids)
+
+    def __getitem__(self, idx: int) -> dict[str, torch.Tensor]:
+        sample_id = self.sample_ids[idx]
+        sample_dir_path = self.dataset_dir_path / sample_id
+        inputs = torch.load(sample_dir_path / "inputs.pt")
+        outputs = torch.load(sample_dir_path / "outputs.pt")
+        return {
+            "inputs": inputs,
+            "outputs": outputs,
+        }
+
+    def get_ids_from_indices(self, indices: tuple[int, ...]) -> tuple[str, ...]:
+        return tuple(self.sample_ids[idx] for idx in indices)
+
     def get_indices_from_ids(self, ids: tuple[str, ...]) -> tuple[int, ...]:
-        ...
+        contained_ids = set([d.name for d in self.sim_dirs]).intersection(ids)
+        return tuple(self.sample_ids.index(id) for id in contained_ids)
