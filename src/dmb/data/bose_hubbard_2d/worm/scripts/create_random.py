a4b65079804c0829bc01365bfe1ac2d2378c6dc9
Fabian Dechent <dechent.fabian@gmail.com>
2024-11-09 16:42:38 +0100

    feat: add slurm job scripts task file

--- src/dmb/data/bose_hubbard_2d/worm/scripts/create_random.py
+++ src/dmb/data/bose_hubbard_2d/worm/scripts/create_random.py
@@ -10,8 +10,11 @@ import numpy as np
 from dotenv import load_dotenv
 
 from dmb.data.bose_hubbard_2d.potential import get_random_trapping_potential
-from dmb.data.bose_hubbard_2d.worm.simulation import WormInputParameters, \
-    WormSimulation, WormSimulationRunner
+from dmb.data.bose_hubbard_2d.worm.simulation import (
+    WormInputParameters,
+    WormSimulation,
+    WormSimulationRunner,
+)
 from dmb.data.dispatching import AutoDispatcher
 from dmb.logging import create_logger
 from dmb.paths import REPO_DATA_ROOT
